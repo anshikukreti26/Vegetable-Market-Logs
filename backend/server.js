4fb201daa6d97f575d1cdcf5764976fb67f0d0ee
Anshika Kukreti <anshikukreti26@gmail.com>
2023-09-11 19:35:12 +0530

    ajax to connect to dataset.json

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const path = require('path'); // Import the path module
-
+const path = require('path');
 const app = express();
-
 const bodyParser = require('body-parser');
-
-const { exec } = require('child_process');
 const fs = require('fs');
+const pickle = require('pickle'); // Import the pickle module
 
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://anshikukreti26:130519Mk@vegetable-market-log.ixa5dvw.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
-  console.log("connection successfull")
+  console.log("Connection successful");
 }).catch((e) => {
-  console.log("no connection")
+  console.log("No connection");
 });
 
 // Create User Schema
@@ -39,36 +36,7 @@ app.set("view engine", "hbs");
 app.set("views", templatePath);
 
 // Define your routes
-
-// Handle user registration
-app.get("/", (req, res) => {
-  res.render("login")
-});
-
-app.get("/register", (req, res) => {
-  res.render("register")
-});
-
-app.post('/register', async (req, res) => {
-  // ... (existing code for registration)
-});
-
-// Handle user login
-app.get("/login", (req, res) => {
-  res.render("login")
-});
-
-app.post('/login', async (req, res) => {
-  // ... (existing code for login)
-});
-
-app.get("/about", (req, res) => {
-  res.render("about")
-});
-
-app.get("/payment", (req, res) => {
-  res.render("payment")
-});
+// ... (existing routes)
 
 // Start the server
 app.listen(3005, () => {
@@ -84,21 +52,49 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-// Add a route to handle predictions
-app.post('/predict', (req, res) => {
-  // ... (existing code for predictions)
+// Load the trained machine learning model
+let model;
+fs.readFile('trained_model.pkl', (err, data) => {
+  if (err) {
+    console.error(`Error reading trained_model.pkl: ${err}`);
+  } else {
+    model = pickle.loads(data);
+    console.log('Machine learning model loaded');
+  }
 });
 
-// Add a route to fetch vegetable names from dataset.json
-app.get('/vegetables', (req, res) => {
-  fs.readFile('dataset.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error(`Error reading dataset.json: ${err}`);
-      res.status(500).json({ error: 'An error occurred while fetching vegetable names.' });
-    } else {
-      const vegetables = JSON.parse(data).map(entry => ({ name: entry.Commodity }));
-      console.log(vegetables);
-      res.json({ vegetables });
-    }
-  });
+// Add a route to handle predictions based on user input
+app.post('/predict', (req, res) => {
+  // Get the vegetable name from the request body
+  const vegetableName = req.body.vegetableName;
+
+  // Ensure that the model is loaded
+  if (!model) {
+    return res.status(500).json({ error: 'Machine learning model not loaded.' });
+  }
+
+  // Predict the price using the loaded model
+  const predictedPrice = predictPrice(vegetableName);
+
+  // Send the predicted price as a response
+  res.json({ predictedPrice });
 });
+
+// Function to predict price for a given vegetable name
+function predictPrice(vegetableName) {
+  if (!model) {
+    console.error('Machine learning model not loaded.');
+    return null;
+  }
+
+  // Create a DataFrame with the same columns as X_encoded
+  const input_data_encoded = {
+    // Set the appropriate key for the vegetable name to 1
+    [`Commodity_${vegetableName}`]: 1,
+  };
+
+  // Predict the price
+  const predictedPrice = model.predict(input_data_encoded);
+
+  return predictedPrice[0];
+}
